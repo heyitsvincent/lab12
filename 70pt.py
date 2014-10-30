c373c9ba2682e9e023102da31621de546b6fd638
heyitsvincent <vincent@1-pps.com>
2014-10-30 15:00:29 -0500

    70 points

--- 70pt.py
+++ 70pt.py
@@ -28,6 +28,9 @@ class MyApp:
 		self.button3.configure(text="Test?", background="cyan")  
 		self.button3.grid(row=0,column=2)	  ### (3)
 			
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="this is for 70 points?", background="maroon")  
+		self.button4.grid(row=0,column=3)	  ### (3)
 	
 		
 root = Tk()
